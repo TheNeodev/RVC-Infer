dad02622ff113af2bd9f740476319b993b2bf886
Neo <neoforevershog@gmail.com>
2025-01-13 00:50:38 +0700

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -1,31 +1,9 @@
 import os
-from pathlib import Path
-import requests
-
-RVC_DOWNLOAD_LINK = "https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/"
-
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-def dl_model(link, model_name, dir_path):
-    url = f"{link}{model_name}"
-    destination = dir_path / model_name
-    dir_path.mkdir(parents=True, exist_ok=True)  # Correct way to ensure directories exist
-    
-    with requests.get(url, stream=True) as r:  # stream=True for large files
-        try:
-            r.raise_for_status()
-            with open(destination, "wb") as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-            print(f"Downloaded {model_name} to {destination}")
-        except requests.exceptions.RequestException as e:
-            print(f"Failed to download {model_name}: {e}")
-    
+hubert_path = "./assets/rvc/hubert/hubert_base.pt"
+rmvpe_path = "./assets/rvc/rmvpe/rmvpe.pt"
 
-if __name__ == "__main__":
-    print("Downloading hubert_base.pt...")
-    dl_model(RVC_DOWNLOAD_LINK, "hubert_base.pt", BASE_DIR / "assets/rvc/hubert")
-    print("Downloading rmvpe.pt...")
-    dl_model(RVC_DOWNLOAD_LINK, "rmvpe.pt", BASE_DIR / "assets/rvc/rmvpe")
-    print("All models downloaded!")
+os.system(f"wget https://huggingface.co/theNeofr/rvc-base/resolve/main/hubert_base.pt -O {hubert_path}")
+os.system(f"wget https://huggingface.co/theNeofr/rvc-base/resolve/main/rmvpe.pt -O {rmvpe_path}")
+print("success installing hubert and rmvpe...")
